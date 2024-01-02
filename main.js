e561ef982c9de9094b041c72a63ca0a9c6512fb4
N.Tsa <24270865+HumAnTarg3t@users.noreply.github.com>
2024-01-02 15:37:25 +0100

    removed code from main

--- main.js
+++ main.js
@@ -19,19 +19,6 @@ function createWindow() {
       nodeIntegration: false,
       contextIsolation: true,
       preload: path.join(__dirname, "preload.js"),
-      contentSecurityPolicy: `
-        default-src 'self';
-        script-src 'self';
-        style-src 'self' 'unsafe-inline';
-        img-src 'self' data:;
-        font-src 'self' https://fonts.gstatic.com;
-        connect-src 'self';
-        object-src 'none';
-        base-uri 'self';
-        form-action 'self';
-        frame-ancestors 'none';
-        frame-src 'none';
-      `,
     },
   });
 
